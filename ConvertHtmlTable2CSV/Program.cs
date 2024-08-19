48f0c11218e69c219ce9742493c0d8d8c9556042
eissaaskari59 <110898590+eissaaskari59@users.noreply.github.com>
2024-08-19 11:37:53 +0200

    minor update
    minor update

--- ConvertHtmlTable2CSV/Program.cs
+++ ConvertHtmlTable2CSV/Program.cs
@@ -1,9 +1,10 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using TrackItSilentApplicationsFramework;
+
 
 namespace ConvertHtmlTable2CSV
 {
@@ -11,37 +12,38 @@ namespace ConvertHtmlTable2CSV
     {
         static void Main(string[] args)
         {
-            TrackItSilentApplicationsFramework.Util.DateTimeNowCest();
-         
+            string csvstring = ConvertHtmlToCSV("<table><tr><td>value1</td><td>value2</td></tr></table>");
+
+
         }
-        public static string ConvertHtmlToCSV(string result, string excelseparator = ";", string options = "", string lineseperator="")
+        public static string ConvertHtmlToCSV(string htmlinputstring, string excelseparator = ";", string options = "", string lineseperator="")
         {
             if (lineseperator == "") lineseperator = Environment.NewLine;
             try
             {
-                result = result.Replace(excelseparator, "");
-                if (result.IndexOf("<table") >= 0)
+                htmlinputstring = htmlinputstring.Replace(excelseparator, "");
+                if (htmlinputstring.IndexOf("<table") >= 0)
                 {
-                    int st = result.IndexOf("<table");
-                    st = result.IndexOf(">", st);
-                    result = result.Substring(st + 1, result.Length - st - 1);
+                    int st = htmlinputstring.IndexOf("<table");
+                    st = htmlinputstring.IndexOf(">", st);
+                    htmlinputstring = htmlinputstring.Substring(st + 1, htmlinputstring.Length - st - 1);
                 }
-                result = result.Replace("<table>", "");
-                result = result.Replace("th>", "td>");
-                result = result.Replace("<th", "<td");
-                result = result.Replace("<td>", "");
-                result = result.Replace("<tr>", "");
-                result = result.Replace("</tr>", lineseperator);
+                htmlinputstring = htmlinputstring.Replace("<table>", "");
+                htmlinputstring = htmlinputstring.Replace("th>", "td>");
+                htmlinputstring = htmlinputstring.Replace("<th", "<td");
+                htmlinputstring = htmlinputstring.Replace("<td>", "");
+                htmlinputstring = htmlinputstring.Replace("<tr>", "");
+                htmlinputstring = htmlinputstring.Replace("</tr>", lineseperator);
 
-                result = result.Replace("</td>", excelseparator);
-                result = result.Replace(@"<td style='border: 1px solid gray;'>", "");
-                result = result.Replace(@"<td style='border: 1px solid gray'>", "");
-                result = result.Replace(@"<td style='border: 1px solid gray;vertical-align:text-top;'>", "");
-                result = result.Replace(@"<td style='border: 1px solid grayvertical-align:text-top'>", "");
-                result = result.Replace("<h1>", "").Replace("</h1>", lineseperator).Replace("<h2>", "").Replace("</h2>", lineseperator).Replace("<h3>", "").Replace("</h3>", lineseperator);
-                result = result.Replace("<br>", lineseperator);
-                result = result.Replace("<br/>", lineseperator);
-                result = result.Replace("</table>", "");
+                htmlinputstring = htmlinputstring.Replace("</td>", excelseparator);
+                htmlinputstring = htmlinputstring.Replace(@"<td style='border: 1px solid gray;'>", "");
+                htmlinputstring = htmlinputstring.Replace(@"<td style='border: 1px solid gray'>", "");
+                htmlinputstring = htmlinputstring.Replace(@"<td style='border: 1px solid gray;vertical-align:text-top;'>", "");
+                htmlinputstring = htmlinputstring.Replace(@"<td style='border: 1px solid grayvertical-align:text-top'>", "");
+                htmlinputstring = htmlinputstring.Replace("<h1>", "").Replace("</h1>", lineseperator).Replace("<h2>", "").Replace("</h2>", lineseperator).Replace("<h3>", "").Replace("</h3>", lineseperator);
+                htmlinputstring = htmlinputstring.Replace("<br>", lineseperator);
+                htmlinputstring = htmlinputstring.Replace("<br/>", lineseperator);
+                htmlinputstring = htmlinputstring.Replace("</table>", "");
 
             }
             catch
@@ -49,7 +51,7 @@ namespace ConvertHtmlTable2CSV
 
             }
 
-            return result;
+            return htmlinputstring;
         }
     }
 }
